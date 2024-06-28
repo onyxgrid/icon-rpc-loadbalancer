37fbf7c2bb4c05f6cf499d8af6ed0dfaafd4ad11
Paul Rouge <paulrouge@outlook.com>
2024-06-28 15:03:40 +0000

    handle bad request

--- internal/loadbalancer/loadbalancer.go
+++ internal/loadbalancer/loadbalancer.go
@@ -129,9 +129,7 @@ func (lb *LoadBalancer) ForwardRequestWithSSL(Nodes []string, w http.ResponseWri
 			if resp.StatusCode == http.StatusBadRequest {
 				return
 			}
-			else {
-				fmt.Println("failed getting response from node", node, "with status code", resp.StatusCode)
-			}
+			fmt.Println("failed getting response from node", node, "with status code", resp.StatusCode)
 		}
 	}
 	http.Error(w, "All Nodes failed", http.StatusServiceUnavailable)
