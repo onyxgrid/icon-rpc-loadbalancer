e1840bc4ddc2c7687b90403b077ee420b832c414
Paul Rouge <paulrouge@outlook.com>
2024-07-04 14:11:41 +0200

    refactor, use helper func to remove nodes

--- internal/loadbalancer/validators.go
+++ internal/loadbalancer/validators.go
@@ -15,7 +15,7 @@ import (
 // setValidators sets all the validators using the tracker.icon.community api
 func (lb *LoadBalancer) setValidators() error {
 	u := "https://tracker.icon.community/api/v1/governance/preps"
-
+	n := []string{}
 	// Create a new HTTP request
 	req, err := http.NewRequest("GET", u, nil)
 	if err != nil {
@@ -42,19 +42,21 @@ func (lb *LoadBalancer) setValidators() error {
 		ip := validator.(map[string]interface{})["api_endpoint"]
 		if ip != nil && ip != "" {
 			if strings.HasPrefix(ip.(string), "http") || strings.HasPrefix(ip.(string), "https") {
-				lb.Nodes = append(lb.Nodes, ip.(string))
+				n = append(n, ip.(string))
 			} else {
-				lb.Nodes = append(lb.Nodes, "http://"+ip.(string)+"/api/v3")
+				n = append(n, "http://"+ip.(string)+"/api/v3")
 			}
 		}
 	}
 
+	lb.mtx.Lock()
+	lb.Nodes = n
+	lb.mtx.Unlock()
 	return nil
 }
 
 // checkNode checks if the node is healthy, if not, remove it from the lb.Nodes list
 func (lb *LoadBalancer) CheckNodes() {
-	// todo | maybe run getValidators here instead of at main func
 	err := lb.setValidators()
 	if err != nil {
 		//todo log
@@ -78,55 +80,31 @@ func (lb *LoadBalancer) CheckNodes() {
 			// Encode the body to JSON
 			bodyBytes, err := json.Marshal(body)
 			if err != nil {
-				lb.mtx.Lock()
-				for i, node := range lb.Nodes {
-					if node == addr {
-						lb.Nodes = append(lb.Nodes[:i], lb.Nodes[i+1:]...)
-					}
-				}
-				lb.mtx.Unlock()
+				// todo | log
 			}
 
 			// Create a new HTTP request
 			req, err := http.NewRequestWithContext(ctx, "POST", addr, bytes.NewBuffer(bodyBytes))
 			if err != nil {
-				fmt.Println("error creating request:", err)
-				lb.mtx.Lock()
-				for i, node := range lb.Nodes {
-					if node == addr {
-						lb.Nodes = append(lb.Nodes[:i], lb.Nodes[i+1:]...)
-					}
-				}
-				lb.mtx.Unlock()
+				// todo 
+				lb.removeNode(addr)
 			}
 			req.Header.Set("Content-Type", "application/json")
 
 			start := time.Now()
 			resp, err := http.DefaultClient.Do(req)
 			if err != nil {
-				// remove node from list
-				lb.mtx.Lock()
-				for i, node := range lb.Nodes {
-					if node == addr {
-						lb.Nodes = append(lb.Nodes[:i], lb.Nodes[i+1:]...)
-					}
-				}
-				lb.mtx.Unlock()
+				// todo | log
+				lb.removeNode(addr)
 				return
 			}
 			defer resp.Body.Close()
-			lb.NodeTimes[addr] = time.Since(start)
+			lb.NodeTimes.Set(addr, time.Since(start))
 
 			var res map[string]interface{}
 			if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
-				// remove node from list
-				lb.mtx.Lock()
-				for i, node := range lb.Nodes {
-					if node == addr {
-						lb.Nodes = append(lb.Nodes[:i], lb.Nodes[i+1:]...)
-					}
-				}
-				lb.mtx.Unlock()
+				// todo | log
+				lb.removeNode(addr)
 				return
 			}
 
@@ -135,23 +113,15 @@ func (lb *LoadBalancer) CheckNodes() {
 			bn.SetString(total, 0)
 			bn.Div(bn, big.NewInt(1000000000000000000))
 
-			// if total supply is 0, remove node from lb.Nodes
 			if bn.Cmp(big.NewInt(0)) == 0 || resp.StatusCode != http.StatusOK {
-				// remove the node from the lb.Nodes list
-				lb.mtx.Lock()
-				for i, node := range lb.Nodes {
-					if node == addr {
-						lb.Nodes = append(lb.Nodes[:i], lb.Nodes[i+1:]...)
-					}
-				}
-				lb.mtx.Unlock()
+				// todo | log
+				lb.removeNode(addr)
 			}
 
 		}(addr)
 	}
 	wg.Wait()
 
-	//todo | order lb.Nodes by the response times in lb.Nodetimes
-
+	lb.sortNodes()
 	fmt.Printf("%d healthy lb.Nodes\n", len(lb.Nodes))
 }
